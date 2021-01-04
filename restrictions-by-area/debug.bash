3ce69a1e973e0306e10faedaf9419bd48ced67fe
Sven Latham (Noggin) <sven@noggin.bi>
2021-01-04 16:30:44 +0000

    Remove working file after completion

--- restrictions-by-area/debug.bash
+++ restrictions-by-area/debug.bash
@@ -3,3 +3,4 @@ cp local_restrictions.yml debugger/
 cd debugger
 docker build -q -t svenlatham/covid-19-debugger . >/dev/null
 docker run --rm svenlatham/covid-19-debugger >../local_restrictions.yml
+rm local_restrictions.yml
