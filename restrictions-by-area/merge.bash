ea49b4c261fb5aeecfb690ddd0b4927db488d0e3
Sven Latham (Noggin) <sven@noggin.bi>
2021-01-04 16:47:13 +0000

    Renamed debugger to merger for sanity

--- restrictions-by-area/merge.bash
+++ restrictions-by-area/merge.bash
@@ -2,10 +2,14 @@
 # Merges another YAML file into our main one (useful for historical entries)
 # Note formats must be the same!
 
-
+# 1. Merge the two YAML files (changes.yml should contain an updated version for merging)
 cp local_restrictions.yml working.yml
 docker run --rm -v "${PWD}":/workdir mikefarah/yq eval-all 'select(fi == 0) *+ select(fi == 1)' working.yml changes.yml >local_restrictions.yml
 rm working.yml
-./debug.bash
 
-# Sven is no expert in YAML. Any idea how to deduplicate or sort by date is welcome!
+# 2. Hacky tidy-up script
+cp local_restrictions.yml merger/
+cd merger
+docker build -q -t svenlatham/covid-19-merger . >/dev/null
+docker run --rm svenlatham/covid-19-merger >../local_restrictions.yml
+rm local_restrictions.yml
