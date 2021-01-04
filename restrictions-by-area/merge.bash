3c851041a276bba863bdda43532e05d2191679ec
Sven Latham (Noggin) <sven@noggin.bi>
2021-01-04 16:29:31 +0000

    Added debug to merge process

--- restrictions-by-area/merge.bash
+++ restrictions-by-area/merge.bash
@@ -6,5 +6,6 @@
 cp local_restrictions.yml working.yml
 docker run --rm -v "${PWD}":/workdir mikefarah/yq eval-all 'select(fi == 0) *+ select(fi == 1)' working.yml changes.yml >local_restrictions.yml
 rm working.yml
+./debug.bash
 
 # Sven is no expert in YAML. Any idea how to deduplicate or sort by date is welcome!
